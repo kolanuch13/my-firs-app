66b96ffa6b120a4cdeac4e94938b1e1b2b6c3a55
Mykola <kolanuch13@gmail.com>
2023-06-08 15:43:55 +0300

    Change file name.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "https://kolanuch13.github.io/my-first-app/",
+  publicPath: "https://kolanuch13.github.io/vue-app-go-home/",
 });
