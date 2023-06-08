21aef69becfa30774565a30ff7dc44f45bc446db
Mykola <kolanuch13@gmail.com>
2023-06-08 16:06:17 +0300

    Improvize//

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "https://kolanuch13.github.io/vue-app-go-home/",
+  publicPath: "https://kolanuch13.github.io/vue-app-go-home",
 });
